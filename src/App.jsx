acd75d36a630589346e6ba48894e3506c7997d9c
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-09-18 21:44:42 +0200

    react world tour

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './assets/components/countries';
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>React world tour</h1>
+      <Countries></Countries>
+
+    </>
+  )
+}
+
+export default App
